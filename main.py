d875dbed097a28440c157c483a5b25704c53993d
zhang5611 <870662098@qq.com>
2020-12-25 09:03:38 +0800

    realize select_sort

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 import random
 
-### Quick_sort
 
 def quick_sort(arr, l, r):
+    '''
+    Quick_sort
+    '''
     if l>=r: return
     low = l
     high = r
@@ -21,9 +23,21 @@ def quick_sort(arr, l, r):
     quick_sort(arr, low, l-1)
     quick_sort(arr, l+1, high)
 
+def select_sort(arr):
+    for i in range(len(arr)-1):
+        min_idx = i
+        for j in range(i, len(arr)-1):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+
 
 if __name__ == "__main__":
-    arr = np.random.randint(10,size=10)
+    arr = np.random.randint(10,size=20)
     print(arr)
-    quick_sort(arr, 0, len(arr)-1)
+    # quick_sort(arr, 0, len(arr)-1)
+    select_sort(arr)
     print(arr)
